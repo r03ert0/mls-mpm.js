a7dbce2bb0285d602d33599e829e1dc692de2e63
r03ert0 <rto@braincatalogue.org>
2019-03-23 15:15:54 +0100

    add folding simulation

--- folding.js
+++ folding.js
@@ -5,15 +5,25 @@ const dt = 1e-4; // time step for simulation
 const dx = 1.0 / n; // cell width
 const inv_dx = 1.0 / dx; // number of cells as a real number
 
-// material constants
+// common material constants
 const particle_mass = 1.0;
 const vol = 1.0; // particle volume
-const hardening = 10.0; // hardening constant for snow plasticity under compression
-const E = 1e+4; // Young's modulus
-const nu = 0.2; // Poisson's ratio
-const mu_0 = E / (2 * (1 + nu)); // Shear modulus (or Dynamic viscosity in fluids)
-const lambda_0 = E * nu / ((1+nu) * (1 - 2 * nu)); // Lamé's 1st parameter \lambda=K-(2/3)\mu, where K is the Bulk modulus
-const plastic = 1; // whether (1=true) or not (0=false) to simulate plasticity
+
+// particular material constants
+const CC = [];
+CC[0x168587] = { // white matter, soft
+    E: 100, // Young's modulus
+    nu: 0.2 // Poisson's ratio
+}
+CC[0xED553B] = { // grey matter, harder
+    E: 3000, // Young's modulus
+    nu: 0.2 // Poisson's ratio
+}
+for (let c in CC) {
+    const {E, nu} = CC[c];
+    CC[c].mu = E / (2 * (1 + nu)); // Shear modulus (or Dynamic viscosity in fluids)
+    CC[c].lambda = E * nu / ((1+nu) * (1 - 2 * nu)); // Lamé's 1st parameter \lambda=K-(2/3)\mu, where K is the Bulk modulus
+}
 
 function Particle(x, c) {
     return {
@@ -29,11 +39,11 @@ function Particle(x, c) {
 const particles = [];
 const grid = []; // velocity + mass, node_res = cell_res + 1
 
-function gridIndex(i, j) { return i + (n+1)*j; }
+function gridIndex(i, j, k) { return i + (n+1)*j; }
 
 function advance(dt) {
     // Reset grid
-    for(let i = 0; i < (n+1)*(n+1); i++) {
+    for(let i = 0; i < (n+1)**2; i++) {
         grid[i] = [0,0,0];
     }
 
@@ -49,21 +59,16 @@ function advance(dt) {
             had2D([0.5, 0.5], sub2D(fx, [0.5, 0.5]).map(o=>o*o))
         ];
 
-        // Snow-like hardening
-        const e = Math.exp(hardening * (1.0 - p.Jp));
-        const mu=mu_0*e;
-        const lambda=lambda_0*e;
-
         // Cauchy stress times dt and inv_dx
         // original taichi: stress = -4*inv_dx*inv_dx*dt*vol*( 2*mu*(p.F-r)*transposed(p.F) + lambda*(J-1)*J )
         // (in taichi matrices are coded transposed)
+        const {mu, lambda} = CC[p.c];
         const J = determinant(p.F); // Current volume
-        const {R:r, S:s} = polar_decomp(p.F); // Polar decomp. for fixed corotated model
+        const {R:r, S:s} = polar_decomp(p.F);
         const k1 = -4*inv_dx*inv_dx*dt*vol;
         const k2 = lambda*(J-1)*J;
         const stress = addMat( mulMat(subMat(transposed(p.F),r),p.F).map(o=>o*2*mu), [k2,0,0,k2] ).map(o=>o*k1);
         const affine = addMat(stress, p.C.map(o=>o*particle_mass));
-
         const mv = [p.v[0]*particle_mass, p.v[1]*particle_mass, particle_mass]; // translational momentum
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) { // scatter to grid
@@ -82,25 +87,20 @@ function advance(dt) {
             const ii = gridIndex(i, j);
             if (grid[ii][2] > 0) { // no need for epsilon here
                 grid[ii] = grid[ii].map(o=>o/grid[ii][2]); // normalize by mass
-                grid[ii] = add3D(grid[ii], [0,-200*dt,0]); // add gravity
                 const x = i/n;
                 const y = j/n; // boundary thickness, node coord
 
                 // stick
-                if (x < boundary||x > 1-boundary||y > 1-boundary) {
+                if (x < boundary || x > 1-boundary || y > 1-boundary || y < boundary) {
                     grid[ii]=[0,0,0];
-                }
-
-                // separate
-                if (y < boundary) {
-                    grid[ii][1] = Math.max(0.0, grid[ii][1]);
                 }
             }
         }
     }
 
     // 2. Grid to particle
-    for (let p of particles) {
+    for (let ind = 0; ind<particles.length; ind ++) {
+        const p = particles[ind];
         const base_coord=sub2D(p.x.map(o=>o*inv_dx),[0.5,0.5]).map(o=>parseInt(o));// element-wise floor
         const fx = sub2D(sca2D(p.x, inv_dx), base_coord); // base position in grid units
         const w = [
@@ -125,25 +125,34 @@ function advance(dt) {
 
         // MLS-MPM F-update
         // original taichi: F = (Mat(1) + dt * p.C) * p.F
-        let F = mulMat(p.F, addMat([1,0, 0,1], p.C.map(o=>o*dt)));
-
-        // Snow-like plasticity
-        let {U:svd_u, sig:sig, V:svd_v} = svd(F);
-        for (let i = 0; i < 2 * plastic; i++) {
-            sig[i+2*i] = clamp(sig[i+2*i], 1.0 - 2.5e-2, 1.0 + 7.5e-3);
-        }
-        const oldJ = determinant(F);
-        // original taichi: F = svd_u * sig * transposed(svd_v)
-        F = mulMat(mulMat(svd_u, sig), transposed(svd_v));
-        const Jp_new = clamp(p.Jp * oldJ / determinant(F), 0.6, 20.0);
-        p.Jp = Jp_new;
-        p.F = F;
+        p.F = mulMat(p.F, addMat([1,0, 0,1], p.C.map(o=>o*dt)));
+        p.Jp = determinant(p.F);
     }
 }
 
-function add_rnd_square(center, c) {
-    for (let i = 0; i < 1000; i++) {
-        // Randomly sample 1000 particles in the square
-        particles.push(new Particle(add2D([(Math.random()*2-1)*0.08, (Math.random()*2-1)*0.08], center), c));
+function add_rnd_disc(center, minRadius, maxRadius, c) {
+  let n = 100000; // maximum number of particles (for a disc without hole)
+  let i=0;
+  let p, x;
+  let a=Math.PI*(maxRadius**2-minRadius**2); // surface area of the disc
+  n=parseInt(a*n); // actual number of particles
+  while(i < n) {
+    x = [2*Math.random()-1, 2*Math.random()-1];
+    p = x[0]**2 + x[1]**2;
+    if(p<maxRadius*maxRadius && p>minRadius*minRadius) {
+      particles.push(new Particle(add2D(x, center), c));
+      i++;
     }
+  }
+}
+
+function add_disc(center, minRadius, maxRadius, c) {
+  const res = 0.005;
+  for(let i=-maxRadius;i<maxRadius;i+=res) {
+    for(let j=-maxRadius;j<maxRadius;j+=res) {
+      if(i**2 + j**2<maxRadius**2 && i**2 + j**2>minRadius**2) {
+        particles.push(new Particle(add2D([i, j], center), c));
+      }
+    }
+  }
 }
